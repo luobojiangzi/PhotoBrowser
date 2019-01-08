946c2a4b28e8331cd194a9a72c48b642e2f9870b
luobojiangzi <1244542549@qq.com>
2019-01-08 18:45:43 +0800

    Initial Commit

---
+++ PhotoBrowserView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PhotoBrowserView
+//
+//  Created by zhihuili on 2019/1/8.
+//  Copyright © 2019 智慧  李. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ PhotoBrowserView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoBrowserView
+//
+//  Created by zhihuili on 2019/1/8.
+//  Copyright © 2019 智慧  李. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
